cb5475cd85248a05803502b9690c279110b4a511
Jian Li <lijianxp2005@gmail.com>
2017-04-09 14:17:55 -0400

    add GCJ 2016 Round 2: Problem A, B and C

---
+++ 2016/2/D/D.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+#include <set>
+#include <map>
+
+using namespace std;
+
+
+void solve() {
+}
+
+int main() {
+    int T;
+    int test = 1;
+    for (cin >> T; T --;) {
+        cin >> S;
+        cout << "Case #" << test ++ << ": ";
+        solve();
+    }
+    return 0;
+}
